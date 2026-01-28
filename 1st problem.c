cdf7073a2159e0581d9543c56295d821dbbc232e
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-28 17:56:12 +0530

    Add files via upload

--- 1st problem.c
+++ 1st problem.c
@@ -1,31 +1,92 @@
-#include <stdio.h>
+// #include <stdio.h>
+//
+// int main() {
+//     int n;
+//     scanf("%d",&n);
+//     if (1<=n && n<=1000) {
+//         for (int i=1;i<=n;i++) {
+//                 printf("%d ",i);
+//         }
+//     }
+//     return 0;
+// }
 
+// #include <stdio.h>
+//
+// int main() {
+//     int n;
+//     int fact = 1;
+//
+//     scanf("%d", &n);
+//
+//     for (int i = 1; i <= n; i++) {
+//         fact  = fact*i;
+//     }
+//
+//     printf("%d", fact);
+//
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main() {
+//     int n;
+//     int sum=0;
+//     scanf("%d",&n);
+//     if (1<=n && n<=1000) {
+//         for (int i=1;i<=n;i++) {
+//             if (i%2==0) {
+//                 sum=sum+i;
+//             }
+//
+//         }
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
+#include<stdio.h>
 int main() {
-    int n,p;
-    int bill=0;
-    scanf("%d %d",&n,&p);
-    switch(n){
-        case 1:
-        if(p<=100){
-            bill=p*3;
-            printf("%d",bill);
-        }
-        else if(p>100){
-            p=p-100;
-            bill=(300+(p*5));
-            printf("%d",bill-80);
-        }
-        break;
-        case 2:
-        if(p<=100){
-            bill=(bill*7);
-            printf("%d",bill);
-        }
-        else if(p>100){
-            p=p-100;
-            bill=(700+(p*10));
-            printf("%d",bill);
-        }
+    int n;
+    int ld;
+    scanf("%d",&n);
+    for (int i=0;i<=n;i++) {
+        ld=(n%10);
+        printf("%d",ld);
     }
+
     return 0;
 }
+
+#include<stdio.h>
+int main() {
+    int n;
+    int ld=0;
+    scanf("%d",&n);
+    for (int i=0;i<=n;i++) {
+
+        ld=(i%10);
+
+    }
+    printf("%d",ld);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
